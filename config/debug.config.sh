e433681650e784221343788d9e4b1882c59ea6e8
Jan_Hybs <jan.hybs@tul.cz>
2016-08-08 12:50:49 +0200

    Add config files

---
+++ config/debug.config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd @PETSC_INSTALL_DIR@/src
+
+./configure \
+        PETSC_ARCH=linux-debug \
+        --with-cc=@CMAKE_C_COMPILER@ --with-cxx=@CMAKE_CXX_COMPILER@ --with-fc=@CMAKE_Fortran_COMPILER@ \
+        --download-fblaslapack --download-mpich --download-metis=yes --download-parmetis=yes \
+        --with-debugging=1 --with-shared-libraries=0 \
+        --with-make-np --CFLAGS="-O3 -DNDEBUG" --CXXFLAGS="-O3 -DNDEBUG -Wall -Wno-unused-local-typedefs -std=c++11"
